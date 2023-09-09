25fc1bfa87f4a4e56d766b3da298f0185ef6b71e
Ayeshamza <125600950+Ayeshamza@users.noreply.github.com>
2023-09-09 16:00:27 +0500

    change

---
+++ client/src/App.js
@@ -0,0 +1,64 @@
+
+import { useEffect, useRef , useState} from 'react'
+import './App.css';
+import { uploadFile } from './services/api';
+
+
+
+
+function App() {
+
+  const[ file, setFile]= useState('')
+  const [result, setResult]= useState('')
+
+  const fileInputRef = useRef()
+
+
+  const url = "https://hips.hearstapps.com/hmg-prod/images/nature-quotes-landscape-1648265299.jpg?crop=0.676xw:1.00xh;0.148xw,0&resize=640:*"
+
+
+
+  useEffect(() => {
+    const getImage = async () => {
+      if (file) {
+        const data = new FormData();
+        data.append("name", file.name);
+        data.append("file", file);
+
+        const response = await uploadFile(data);
+        setResult(response.path);
+      }
+    }
+    getImage();
+  }, [file])
+
+  const onUploadClick = () => {
+    fileInputRef.current.click();
+  }
+
+
+
+ 
+
+ return (
+  <div className='container'>
+    <img src={url} className='img' />
+    <div className='wrapper'>
+      <h1>Simple file sharing!</h1>
+      <p>Upload and share the download link.</p>
+      
+      <button onClick={() => onUploadClick()}>Upload</button>
+      <input
+        type="file"
+        ref={fileInputRef}
+        style={{ display: "none" }}
+        onChange={(e) => setFile(e.target.files[0])}
+      />
+
+      <a href={result} target='_blank'>{result}</a> 
+    </div>
+  </div>
+);
+}
+
+export default App;
